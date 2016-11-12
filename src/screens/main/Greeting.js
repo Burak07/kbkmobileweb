d2cbd4cf756f7c9788818deeaf127e3014b80f0d
Burak07 <burak_karasoy@hotmail.com>
2016-11-12 18:51:56 +0300

    'a'

---
+++ src/screens/main/Greeting.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Greeting extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      isChecked:false,
+    }
+ }
+
+
+   render() {
+    return(
+    <div>
+      <h1>abcde</h1>
+    </div>);
+ }
+}
+export default Greeting
